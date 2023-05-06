80be91eb2281cc8c6f5800c856ae3da925aee830
Higor Ch <higorchristianfe@gmail.com>
2023-05-06 18:15:14 -0300

    dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,6 @@ module.exports = {
   theme: {
     extend: {
       height: {
-        "side-body": "calc(100vh - 180px)",
-        "side-header": "90px",
-        "side-footer": "90px",
         "content-header": "70px",
         "content-footer": "75px",
       },
@@ -15,9 +12,8 @@ module.exports = {
         "content-body": "calc(100vh - 145px)",
       },
       colors: {
-        "theme-gray": {
-          "primary": "#f1f3f4",
-          "secondary": "#d9dbdc",
+        "theme": {
+          "primary": "#f1f3f4"
         }
       }
     },
