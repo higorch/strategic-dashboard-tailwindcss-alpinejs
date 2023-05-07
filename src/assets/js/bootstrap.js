36473d19dc2c0283fd822d03fc2a874c30ab4a15
Higor Ch <higorchristianfe@gmail.com>
2023-05-07 12:39:24 -0300

    dashboard

--- src/assets/js/bootstrap.js
+++ src/assets/js/bootstrap.js
@@ -3,7 +3,9 @@ import Alpine from 'alpinejs';
 import 'simplebar';
 import 'simplebar/dist/simplebar.css';
 
-import '../../../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+import '../css/style.css'
 
 window.Alpine = Alpine;
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,39 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/assets/js/bootstrap.js',
     output: {
         path: path.resolve(__dirname, './src/assets/bundle'),
-        filename: 'bundle.js'
+        filename: 'app.js'
     },
     module: {
         rules: [
             {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader'
+                ],
             },
         ],
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'app.css'
+        })
+    ]
 };
