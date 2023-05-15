44fdcc4699397bb64d65d6a175eff7bff9844a7b
Higor Ch <higorchristianfe@gmail.com>
2023-05-15 14:42:43 -0300

    diagram

--- src/assets/js/diagram.js
+++ src/assets/js/diagram.js
@@ -1,15 +1,28 @@
 import { Node, Canvas } from 'butterfly-dag';
 import 'butterfly-dag/pack/index.css';
 
+function setComponent(component) {
+    const element = document.createElement('div');
+    element.innerHTML = component.trim();
+
+    return element.firstChild;
+}
+
 class BaseNode extends Node {
     draw(obj) {
 
-        const novoElemento = document.createElement('div');
-        novoElemento.innerText = obj.options.desc;
-        novoElemento.id = obj.options.id;
-        novoElemento.className = `absolute w-[250px] h-[75px] bg-blue-600`;
+        var component = `
+            <div class="flex flex-col absolute w-[230px] rounded-md bg-slate-100 dark:bg-slate-800 shadow-md shadow-slate-300 dark:shadow-neutral-950">
+                <div class="flex px-[15px] py-[15px]">
+                    <h3 class="text-base font-semibold text-slate-500 dark:text-slate-400">${obj.options.title}</h3>
+                </div>
+                <div class="flex px-[15px] py-[5px] border-t-[1px] border-slate-200 dark:border-slate-700">
+                    <small class="text-slate-500 dark:text-slate-400">321</small>
+                </div>
+            </div>
+        `;
 
-        return novoElemento;
+        return setComponent(component);
     }
 }
 
@@ -40,19 +53,29 @@ document.addEventListener('alpine:init', () => {
         },
         add() {
 
-            this.canvas.addNode({
-                id: 'node-' + this.canvas.nodes.length,
-                text: `Aqui é um texto`,
-                Class: BaseNode,
-                condition: 'and',
-                desc: 'Aqui é uma descrição',
-                endpoints: [{
+            var endpoints = [];
+
+            if (this.canvas.nodes.length === 0) {
+                endpoints = [{
+                    id: 'bottom',
+                    orientation: [0, 1]
+                }];
+            } else {
+                endpoints = [{
                     id: 'top',
                     orientation: [0, -1]
                 }, {
                     id: 'bottom',
                     orientation: [0, 1]
-                }]
+                }];
+            }
+
+            this.canvas.addNode({
+                id: 'node-' + this.canvas.nodes.length,
+                Class: BaseNode,
+                title: `Aqui é um texto`,
+                desc: 'Aqui é uma descrição',
+                endpoints: endpoints
             });
         }
     }));
