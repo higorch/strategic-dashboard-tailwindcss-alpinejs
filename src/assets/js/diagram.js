0a744e866abef5cf4f56d8a43ee11218c01bd766
Higor Ch <higorchristianfe@gmail.com>
2023-05-15 11:05:54 -0300

    diagram

--- src/assets/js/diagram.js
+++ src/assets/js/diagram.js
@@ -1,19 +1,13 @@
-import { TreeNode, TreeCanvas } from 'butterfly-dag';
+import { Node, Canvas } from 'butterfly-dag';
 import 'butterfly-dag/pack/index.css';
 
-class BaseNode extends TreeNode {
-    constructor(opts) {
-        super(opts);
-    }
-    draw = (opts) => {
+class BaseNode extends Node {
+    draw(obj) {
+
         const novoElemento = document.createElement('div');
+        novoElemento.innerText = obj.options.desc;
+        novoElemento.id = obj.options.id;
         novoElemento.className = `absolute w-[250px] h-[75px] bg-blue-600`;
-        novoElemento.style.setProperty('top', opts.top + 'px');
-        novoElemento.style.setProperty('left', opts.left + 'px');
-        novoElemento.setAttribute('x-data', '');
-        novoElemento.setAttribute(':id', "$id('node')");
-        novoElemento.innerText = opts.options.desc;
-        novoElemento.id = opts.id;
 
         return novoElemento;
     }
@@ -28,7 +22,7 @@ document.addEventListener('alpine:init', () => {
         },
         run() {
 
-            this.canvas = new TreeCanvas({
+            this.canvas = new Canvas({
                 root: this.$refs.render,
                 disLinkable: true,
                 linkable: true,
@@ -40,24 +34,6 @@ document.addEventListener('alpine:init', () => {
                         shapeType: 'Manhattan',
                         arrow: true
                     }
-                },
-                layout: {
-                    type: 'compactBox',
-                    options: {
-                        direction: 'TB',
-                        getHeight(d) {
-                            return 60;
-                        },
-                        getWidth(d) {
-                            return 120;
-                        },
-                        getHGap(d) {
-                            return 20;
-                        },
-                        getVGap(d) {
-                            return 80;
-                        },
-                    }
                 }
             });
 
@@ -65,18 +41,17 @@ document.addEventListener('alpine:init', () => {
         add() {
 
             this.canvas.addNode({
+                id: 'node-' + this.canvas.nodes.length,
                 text: `Aqui é um texto`,
                 Class: BaseNode,
                 condition: 'and',
                 desc: 'Aqui é uma descrição',
                 endpoints: [{
                     id: 'top',
-                    orientation: [0, -1],
-                    pos: [0.5, 0]
+                    orientation: [0, -1]
                 }, {
                     id: 'bottom',
-                    orientation: [0, 1],
-                    pos: [0.5, 0]
+                    orientation: [0, 1]
                 }]
             });
         }
